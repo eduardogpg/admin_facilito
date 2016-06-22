3e2f4c7320a68d27d5de4282537be1e0d148f294
Eduardo Ismael <eduardo78d>
2016-06-21 21:55:17 -0500

    vistas basadas en clases fake

--- admin_facilito/clients/views.py
+++ admin_facilito/clients/views.py
@@ -13,17 +13,51 @@ from django.contrib.auth.decorators import user_passes_test
 from forms import LoginForm
 from forms import CreateUserForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.views.generic import View
 
 #decoradores
 def user_authenticated(user):
 	return not user.is_authenticated()
 
 # Create your views here.
-def show(request):
+def show_(request):
 	return HttpResponse("Hola Mundo desde el cliente")
 
+class Show(TemplateView):
+	template_name = "show.html"
+
+class Login(View):
+	form = LoginForm()
+	message = None
+	template = 'login.html'
+
+	def get(self, request, *args, **kwargs):
+		if request.user.is_authenticated():
+			return redirect('client:dashboard')
+		return render( request, self.template,  self.get_context() )
+
+	def post(self, request, *args, **kwargs):
+		username_post = request.POST['username']
+		password_post = request.POST['password']
+		user = authenticate( username = username_post, password = password_post)
+		print "\n\n\n\n\n\n"
+		print type(user)#objecto models
+		if user is not None:
+			login_django(request, user)
+			return redirect('client:dashboard')
+		else:
+			self.message = 'Username o password incorrectos'
+
+		return render( request, 'login.html', self.get_context() )	
+
+	def get_context(self):
+		return { 'form': self.form, 'message' : self.message }
+
+
 @user_passes_test(user_authenticated, login_url = 'client:dashboard')
-def login(request):
+def login_dos(request):
 	message = None
 	if request.method == 'POST':#se envio el formulario
 		username_post = request.POST['username']
@@ -51,10 +85,15 @@ def logout(request):
 	logout_django(request)
 	return redirect('client:login')
 
+#nuevo feature
+class Dashboard(LoginRequiredMixin, View):
+    login_url = 'client:login'
+    
+    def get(self, request, *args, **kwargs):
+    	return render( request, 'dashboard.html', {} )	
 
 def create(request):
 	message = None
-
 	form = CreateUserForm(request.POST or None)
 	if request.method =='POST':
 		if form.is_valid():
