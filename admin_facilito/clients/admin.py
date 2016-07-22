6259fa9735a5cf4c747706475eb589e9eacbfafd
Eduardo Ismael <eduardo78d>
2016-07-22 12:45:23 -0500

    admin

--- admin_facilito/clients/admin.py
+++ admin_facilito/clients/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+from .models import SocialNetwork
+
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+class ClientInline(admin.StackedInline):
+ model = Client
+ can_delete = False
+ #fields = ['job',]
+
+class SocialInline(admin.StackedInline):
+ model = SocialNetwork
+ can_delete = False
+ #fields = ['job',]
+
+class UserAdmin(AuthUserAdmin):
+ inlines = [ClientInline, SocialInline]
+
+#verbose_name_plurase
+class ClientAdmin(admin.ModelAdmin):
+	exclude = ('user',)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Client, ClientAdmin)
--- admin_facilito/clients/models.py
+++ admin_facilito/clients/models.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 
 class Client(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	bio = models.TextField(max_length = 200, default = "" )
-	job = models.CharField(max_length = 50, default = "")
+	bio = models.TextField(max_length = 200, blank=True, default = "" )
+	job = models.CharField(max_length = 50, blank=True, default = "")
 
 	def __str__(self):
 		return self.user.username
 
-#verbose_name_plurase
+class SocialNetwork(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	facebook = models.URLField( blank=True )
+	twitter = models.URLField( blank=True )
+	github = models.URLField( blank=True )
+
+	def __str__(self):
+		return self.user.username
